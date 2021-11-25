1f5526bcbaafb1cf51959030f4aaff291d144280
Henri-Simon <etu40354@henallux.be>
2021-11-25 13:14:30 +0100

    Create table and correct UML

--- src/db/create_table.py
+++ src/db/create_table.py
@@ -26,13 +26,38 @@ def main():
                             health_point integer NOT NULL,
                             critical integer NOT NULL,
                             initiative integer NOT NULL,
-                            team integer NOT NULL,
                             parry integer,
                             dodge integer
-                        ); """
-                        
-    create_table(sql_table_fighter)
+                        );"""
+
+    sql_table_team = """CREATE TABLE IF NOT EXISTS Team (
+                            team_id integer PRIMARY KEY AUTOINCREMENT,
+                            name text NOT NULL
+                        );"""
+
+    sql_table_battle = """CREATE TABLE IF NOT EXISTS Battle (
+                            battle_id integer PRIMARY KEY AUTOINCREMENT,
+                            winner_team text
+                            loser_team text
+                            still_living int
+                        );"""
+
+    sql_table_fighter_team = """CREATE TABLE IF NOT EXISTS Fighter_team (
+                            fighter_team_id integer PRIMARY KEY  AUTOINCREMENT,
+                            fighter_id int NOT NULL,
+                            team_id int NOT NULL,
+                            battle_id int NOT NULL,
+                            FOREIGN KEY (fighter_id) REFERENCES Fighter (fighter_id),
+                            FOREIGN KEY (team_id) REFERENCES Team (team_id),
+                            FOREIGN KEY (battle_id) REFERENCES Battle (battle_id)
+                        );"""
+    
+    querries = [sql_table_fighter, sql_table_team, sql_table_battle, sql_table_fighter_team]
+
+    for i in range(len(querries)):
+        print(i)
+        create_table(querries[i])
 
 if __name__ == '__main__':
     main()
-    
+     
