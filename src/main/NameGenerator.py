8ee4ab368a4b71ef6fc6253d962706cc7785f537
Henri-Simon <etu40354@henallux.be>
2021-11-27 14:53:06 +0100

    Warrior class OK

--- src/main/NameGenerator.py
+++ src/main/NameGenerator.py
@@ -1,10 +1,10 @@
 from random import choices
 
-def Name_generator():
+def name_generator():
     with open ("names.txt","r") as file:
         names = file.readlines()
         name = choices(names)
         return name[0].strip()
 
 if __name__ == '__main__':
-    print(Name_generator())
+    print(name_generator())
--- src/main/Warrior.py
+++ src/main/Warrior.py
@@ -1,8 +1,9 @@
 from Fighter import FighterInterface
 from random import randrange
+from NameGenerator import name_generator
 
 class Warrior(FighterInterface):
-    def __init__(self, name, lastname, attack_value = randrange(70,91), defense_value = randrange(70, 91), health_point = randrange(120, 151), critical = randrange(5,8), initiative = randrange(40,61), parry = randrange(40,61)):
+    def __init__(self, name = name_generator(), lastname = name_generator(), attack_value = randrange(70,91), defense_value = randrange(70, 91), health_point = randrange(120, 151), critical = randrange(5,8), initiative = randrange(40,61), parry = randrange(40,61)):
         super().__init__(name, lastname, attack_value, defense_value, health_point, critical, initiative)
         self.parry = parry
 
@@ -11,8 +12,9 @@ class Warrior(FighterInterface):
 
 
 if __name__ == '__main__':
-    warrior = Warrior("HS", "Lebrun")
+    warrior = Warrior()
     print(warrior.name)
+    print(warrior.lastname)
     print(warrior.attack_value)
     print(warrior.defense_value)
     print(warrior.parry)
